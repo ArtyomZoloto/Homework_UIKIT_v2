4c6a7edaa5f0e5fc51bbc860542fc665ab69e7a3
Artyom Zolotoverkhov <imac@imac-imac.local>
2019-04-20 20:30:13 +0300

    Initial Commit

---
+++ Homework_UIKIT_v2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homework_UIKIT_v2
+//
+//  Created by iMac on 20/04/2019.
+//  Copyright © 2019 Zolotoverkhov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Homework_UIKIT_v2/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Homework_UIKIT_v2
+//
+//  Created by iMac on 20/04/2019.
+//  Copyright © 2019 Zolotoverkhov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
