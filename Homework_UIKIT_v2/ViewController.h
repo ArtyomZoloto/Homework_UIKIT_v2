24cdf17c55a7ba9e80f2c9f024611434d811b23b
Artyom Zolotoverkhov <imac@imac-imac.local>
2019-04-21 17:34:24 +0300

    homework done with incapsulation

--- Homework_UIKIT_v2/ViewController.h
+++ Homework_UIKIT_v2/ViewController.h
@@ -1,11 +1,3 @@
-//
-//  ViewController.h
-//  Homework_UIKIT_v2
-//
-//  Created by iMac on 20/04/2019.
-//  Copyright © 2019 Zolotoverkhov. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
--- Homework_UIKIT_v2/ViewController.m
+++ Homework_UIKIT_v2/ViewController.m
@@ -1,12 +1,5 @@
-//
-//  ViewController.m
-//  Homework_UIKIT_v2
-//
-//  Created by iMac on 20/04/2019.
-//  Copyright © 2019 Zolotoverkhov. All rights reserved.
-//
-
 #import "ViewController.h"
+#import "AZRect.h"
 
 @interface ViewController ()
 
@@ -16,7 +9,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIView *azRect = [[AZRect alloc] initWithFrame:CGRectMake(50, 50, 200, 200)];
+    [self.view addSubview:azRect];
+    
 }
 
 
